562f0cefab0dd3349f77d5be30fbc89d725504ce
ansh batra <marsab@cpan.org>
2011-07-29 13:24:50 +0000

    Import of MARSAB/ANSH from CPAN.
    gitpan-cpan-distribution: ANSH
    gitpan-cpan-version:      
    gitpan-cpan-path:         MARSAB/ANSH.tar.gz
    gitpan-cpan-author:       MARSAB
    gitpan-cpan-maturity:     released

---
+++ lib/ANSH.pm
@@ -0,0 +1,131 @@
+package ANSH;
+
+use 5.010001;
+use strict;
+use warnings;
+require Exporter;
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+ANSH - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use ANSH;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for ANSH, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+=head CONTENTS
+AnyDBM_File
+Attribute::Handlers
+BerkeleyDB
+Carp
+Class::Data::Inheritable
+Class::Singleton
+Compress::Raw::Bzip2
+Compress::Raw::Zlib
+Crypt::OpenSSL::Random
+Crypt::RC4
+Data::Dump
+Date::Format
+Date::Parse
+DB_File
+Digest::Nilsimsa
+Digest::SHA
+DirHandle
+Encode
+Exporter
+FCGI
+File::Find
+File::Glob
+File::Grep
+File::Stat::Bits
+Filter::Util::Call
+GD
+GD::Text
+Geography::Countries
+Geo::IP
+Getopt::Std
+HTML::HTMLDoc
+HTML::Tagset
+Image::Magick
+integer
+IO::Dir
+IO::File
+IO::Scalar
+IO::Socket::INET
+IO::Stringy
+Locale::Maketext::Simple
+LWP::MediaTypes
+Math::Round
+MD5
+MIME::Base64
+MIME::QuotedPrint
+MLDBM::Sync
+mod_perl2
+Net::CIDR::Lite
+Net::Domain
+Net::FTP
+Net::IP
+Net::Ping
+Number::Compare
+OLE::Storage_Lite
+overload
+Pod::Escapes
+Probe::Perl
+Scalar::Util::Numeric
+Storable
+String::CRC32
+Sys::Hostname
+Sys::Hostname::Long
+Term::ReadKey
+Term::ReadLine
+Test
+Test::Harness
+Test::Inter
+Text::Wrap
+Tie::Cache
+Time::Local
+Time::Piece
+vars
+warnings
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+root, E<lt>root@(none)E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by root
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
